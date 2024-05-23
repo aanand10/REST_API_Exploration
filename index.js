d678a6d58eb4f529cde1b867865629f65fce9177
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2024-05-23 15:43:20 +0530

    feat : requests with same route are grouped | feat : added patch and delete logic

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./Data/MOCK_DATA.json");
 
 const app = express();
 const PORT = 1010;
-
+//MiddleWare : plugin
+app.use(express.urlencoded({ extended: false }));
 // routes
 app.get("/users", (req, res) => {
   /**
@@ -22,26 +24,58 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  console.log(id);
-  const userWithId = users.find((user) => user.id === id);
-  return res.json(userWithId);
-});
+// grouping the same route requests
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    console.log(id);
+    const userWithId = users.find((user) => user.id === id);
+    return res.json(userWithId);
+  })
+  .patch((req, res) => {
+    //TODO : edit the user with id
+    const id = Number(req.params.id);
+    const getUserData = users.find((user) => user.id === id);
+    console.log(getUserData);
+    const getUserIndex = users.findIndex((user) => user.id === id);
+    console.log(getUserIndex);
+    const body = req.body;
+    users.splice(getUserIndex, 1, { id: id, ...body });
+    fs.writeFile("/Data/MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.json({
+        status: "Success",
+        id: id,
+      });
+    });
+  })
+  .delete((req, res) => {
+    //TODO : delete the user with id
+    const id = Number(req.params.id);
+    const getUserIndex = users.findIndex((user) => user.id === id);
+    console.log(getUserIndex);
+    users.splice(getUserIndex, 1);
+    fs.writeFile("/Data/MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.json({
+        status: "Success!",
+        id: id,
+      });
+    });
+  });
 
 app.post("/api/users", (req, res) => {
   //TODO : create new user
-  return res.json({ status: "Pending" });
-});
+  const body = req.body;
+  console.log("REQ Body", body);
 
-app.patch("/api/users/:id", (req, res) => {
-  //TODO : edit the user with id
-  return res.json({ status: "Pending" });
-});
+  users.push({
+    id: users.length + 1,
+    ...body,
+  });
 
-app.delete("/api/users/:id", (req, res) => {
-  //TODO : delete the user with id
-  return res.json({ status: "Pending" });
+  fs.writeFile("./Data/MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Success", id: users.length });
+  });
 });
 
 app.listen(PORT, () => {
